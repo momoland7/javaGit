e43326d4e157e28382df6192cd68fe76bfe93880
momoland7 <momoland7@yahoo.co.jp>
2017-04-11 10:47:01 +0900

    타임

--- javaGit/src/com/java/hello/Hello.java
+++ javaGit/src/com/java/hello/Hello.java
@@ -1,6 +1,7 @@
 package com.java.hello;
 
 public class Hello {
+	protected int x=10;
 	public void helloMethod()
 	{
 		System.out.println("Hello world");
--- javaGit/src/com/java/main/HelloTest.java
+++ javaGit/src/com/java/main/HelloTest.java
@@ -2,12 +2,12 @@ package com.java.main;
 
 import com.java.hello.*;
 
-public class HelloTest {
+public class HelloTest extends Hello{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Hello h = new Hello();
-		h.helloMethod();
+		HelloTest ht = new HelloTest();
+		System.out.println(ht.x);
 
 	}
 
