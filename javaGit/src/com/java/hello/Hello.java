9348cfad61e0ac981053dcd83dc08f2c9c20b871
momoland7 <momoland7@yahoo.co.jp>
2017-04-11 09:38:03 +0900

    package test

---
+++ javaGit/src/com/java/hello/Hello.java
@@ -0,0 +1,10 @@
+package com.java.hello;
+
+public class Hello {
+	public void helloMethod()
+	{
+		System.out.println("Hello world");
+	}
+	
+
+}
---
+++ javaGit/src/com/java/main/HelloTest.java
@@ -0,0 +1,14 @@
+package com.java.main;
+
+import com.java.hello.*;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello h = new Hello();
+		h.helloMethod();
+
+	}
+
+}
